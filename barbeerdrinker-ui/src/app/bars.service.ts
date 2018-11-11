1ca2194df3b7e257282e9a820e76f98d551a5cfe
GreedyAlgorithm1 <sk1683@scarletmail.rutgers.edu>
2018-11-11 14:57:09 -0500

    Adding UI again

---
+++ barbeerdrinker-ui/src/app/bars.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Bar{
+	id: number;
+	name: string;
+	state: string;
+	open: number;
+	close: number;
+	license: string;
+	phone: string;
+	address: string;
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BarsService {
+
+  constructor(
+  public http: HttpClient
+  ) { }
+
+getBars(){
+	return this.http.get<Bar[]>('/api/bar');
+}
+
+}
